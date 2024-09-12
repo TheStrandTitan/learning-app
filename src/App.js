eb8fba5c340fc1463c9445d77c7b5afcff3d8e82
Brennen <bkchristensen@gestaltdiagnostics.com>
2024-09-12 16:01:19 -0700

    Implemented Basic state management using localstorage and added .env file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import staticImageSaved from './plainSquare.jpg';
+
+import { useState } from 'react';
+
+function ClickManager() {
+  const [count, clickCheck] = useState(0)
+  function handleClickCheck() {
+      clickCheck(count + 1);
+  }
+
+  return ( 
+    <button onClick={handleClickCheck}>
+      This button has been clicked {count} times.
+    </button>
+  );
+
+}
 
 
 
 function ImageTester() {
+  let count = -1;
+    const refreshVal = Number(localStorage.getItem("incrementRefresh")) || 0;
+    count = refreshVal + 1;
+    localStorage.setItem("incrementRefresh", count);
+
   return (
-  <img className='redSquare' alt='Normal Red Square' src='src/plainSquare.jpg'></img>
+    <div>
+        <img className='redSquare' alt='Normal Red Square' src={staticImageSaved}></img>
+        <h3>This Image has been refreshed {count} times.</h3>
+
+    </div>
   );
 }
 
@@ -18,6 +44,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Practice Test</h1>
+        <ClickManager />
         <h2>Image Test</h2>
         <ImageTester />
       </header>
